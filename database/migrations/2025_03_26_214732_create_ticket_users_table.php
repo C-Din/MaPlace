27ab5e2404eccc149152769f13191c973172a4dc
Cedric Din <35843697+C-Din@users.noreply.github.com>
2025-03-26 17:57:51 -0400

    set models and migrations

---
+++ database/migrations/2025_03_26_214732_create_ticket_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ticket_users', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('ticket_id')->constrained('tickets');
+            $table->foreignId('user_id')->constrained('users');
+            $table->integer('quantity');
+            $table->decimal('total_price', 10, 2);
+            $table->boolean('is_active')->default(true);
+            $table->dateTime('payment_date')->nullable();
+            $table->string('payment_code')->nullable();
+            $table->string('payment_status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ticket_users');
+    }
+};
